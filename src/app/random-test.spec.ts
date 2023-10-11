6dba4bb75e2dc0994171443ce132febc7927ec8d
egeinanc-cc <146418830+egeinanc-cc@users.noreply.github.com>
2023-10-11 08:43:27 +0200

    remove line (#6)
    * remove line
    
    * wip

--- src/app/random-test.spec.ts
+++ src/app/random-test.spec.ts
@@ -2,7 +2,6 @@ import {describe, expect, it} from "@jest/globals";
 
 describe('tmp', () => {
     it('should run test', () => {
-        expect(false).toBeTruthy()
-        expect(false).toBeTruthy()
+        expect(true).toBeTruthy()
     });
 });
