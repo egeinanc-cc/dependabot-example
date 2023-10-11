aed568f9952c54a6512dbbe7f8689bb79d055128
Ege Inanc <ege.inanc@codecentric.de>
2023-10-11 09:47:36 +0200

    wip

--- src/app/random-test.spec.ts
+++ src/app/random-test.spec.ts
@@ -3,5 +3,7 @@ import {describe, expect, it} from "@jest/globals";
 describe('tmp', () => {
     it('should run test', () => {
         expect(true).toBeTruthy()
+        expect(false).toBeTruthy()
+        expect(false).toBeTruthy()
     });
 });
