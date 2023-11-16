f0b6672f72d617d26f640085574df9f9e056a706
Ege Inanc <ege.inanc@codecentric.de>
2023-11-16 13:50:36 +0100

    commit in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+/**
+ * sakldjfbnskaödn
+ * @constructor
+ */
 export default function Home() {
   return (
     <main className={styles.main}>
